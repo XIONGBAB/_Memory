dcba2982739e5ae367750686bb980d8c7488abd8
1028543553@qq.com <2770949518@qq.com>
2025-06-16 20:01:17 +0800

    feat: documnet new layout

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,4 +1,5 @@
-const process = require('node:process');
+import process from 'node:process';
+
 if (!/pnpm/.test(process.env.npm_execpath || '')) {
   console.warn(
     `\u001B[33mThis repository must using pnpm as the package manager `
--- src/views/DocumentPage/index.vue
+++ src/views/DocumentPage/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container">
-    <div class="layout-aside">
-    </div>
-    <div class="layout-header"></div>
-    <div class="layout-main">
+    <div class="doc-nav"></div>
+    <div class="doc-layout">
+      <div class="doc-aside"></div>
+      <div class="doc-content"></div>
     </div>
   </div>
 </template>
@@ -14,9 +14,33 @@
 
 <style scoped lang='scss'>
 .container {
+  display: flex;
+  flex-direction: column;
+}
+// .container {
+//   display: grid;
+//   grid-template-rows: 60px repeat(24, 1fr);
+//   grid-template-columns: repeat(24, 1fr);
+// }
+.doc-nav {
+  flex: 0 0 60px;
+  border: $bd-test;
+}
+.doc-layout {
+  flex: 1;
   display: grid;
   grid-template-rows: repeat(24, 1fr);
-  grid-template-columns: repeat(24, 1fr);
-  border: 1px solid blue;
+  grid-template-columns: 240px 1fr;
+  border: 1px solid red;
+  .doc-aside {
+    grid-row: 1 / -1;
+    grid-column: 1;
+    background: #f0f0f0;
+  }
+  .doc-content {
+    grid-row: 1 / -1;
+    grid-column: 2;
+    background: #e0e0e0;
+  }
 }
 </style>
