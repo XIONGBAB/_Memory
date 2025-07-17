fb20f9546df748c714a5e06a879242f1282b6d02
XIONGBAB <2770949518@qq.com>
2025-07-17 17:00:13 +0800

    fix: fix postCssPxToRem rules

--- src/views/HomePage/index.vue
+++ src/views/HomePage/index.vue
@@ -1,27 +1,11 @@
 <template>
-  <div>{{ $t("button.save") }}</div>
-  <button @click="toggleDark()">切换</button>
-  <button :disabled="locale === 'zh'" @click="handleCommand('zh')">中文</button>
-  <button :disabled="locale === 'en'" @click="handleCommand('en')">英文</button>
+  <div>1</div>
 </template>
 
-<script setup lang="ts" name="HomePage">
-import { useDark, useToggle } from "@vueuse/core";
-import { ElMessage } from "element-plus";
-import { useI18n } from "vue-i18n";
+<script setup lang='ts' name=''>
 
-const { locale } = useI18n();
-
-const isDark = useDark();
-const toggleDark = useToggle(isDark);
-function handleCommand(lang: string) {
-  locale.value = lang;
-  localStorage.setItem("lang", lang);
-  ElMessage.success({
-    message: lang === "zh" ? "语言切换成功" : "Language switched successfully",
-    duration: 1500,
-  });
-}
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang='scss'>
+
+</style>
